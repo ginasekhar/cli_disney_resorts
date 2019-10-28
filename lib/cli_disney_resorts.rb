83e88fa4b71a3a3da6cdde7ae923135d766b37a1
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-28 11:56:22 +0000

    initial commit

---
+++ lib/cli_disney_resorts.rb
@@ -0,0 +1,6 @@
+require "cli_disney_resorts/version"
+
+module CliDisneyResorts
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cli_disney_resorts/version.rb
@@ -0,0 +1,3 @@
+module CliDisneyResorts
+  VERSION = "0.1.0"
+end
